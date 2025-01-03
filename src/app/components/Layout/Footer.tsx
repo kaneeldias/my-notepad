8e2443985a21e3e9708e60db91d1b0d7b4b16ed4
kaneeldias <kaneeldias@hotmail.com>
2025-01-03 11:37:18 +0530

    Add Github repo links

--- src/app/components/Layout/Footer.tsx
+++ src/app/components/Layout/Footer.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function Footer() {
     return (
-        <div className={`bg-gray-100 w-full p-normal border-t-2 border-gray-200`}>
-            <div className={`text-xxs md:text-xs font-light text-right`}>
-                Developed by Kaneel Dias with 💖
+        <div
+            className={`flex flex-row  space-x-large items-center bg-gray-100 w-full p-normal border-t-2 border-gray-200`}>
+            <div className={`text-xxs md:text-xs font-light text-left`}>
+                Developed by <a href={"https://github.com/kaneeldias"} target={"_blank"}>Kaneel Dias</a> with 💖
+            </div>
+            
+            <div>
+                <a href={"https://github.com/kaneeldias/my-notepad"} target={"_blank"}>
+                    <Image src={"/github.svg"} alt={"GitHub"} width={15} height={15}/>
+                </a>
             </div>
         </div>
     );
